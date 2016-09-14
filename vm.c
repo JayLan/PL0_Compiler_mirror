992cd501c8649613b969716f4fefdcd0c7a39cca
Jason Lancaster <jaylan.cs@knights.ucf.edu>
2016-09-14 00:37:22 -0400

    Initial commit for virtual machine

---
+++ vm.c
@@ -0,0 +1,19 @@
+//|===========================================================================
+//|  Team Name: Compiler Builders 33
+//|
+//|  Programmer(s): 
+//|		Cristen Palmer
+//|		Megan Chipman
+//|		Jason Lancaster
+//|		Victoria Proetsch
+//|
+//|  Course:	COP3402, Fall 2016
+//|  Date:
+//|---------------------------------------------------------------------------
+//|  Language:	C
+//|  File:		vm.c
+//|  Purpose:	Assignment #1, Implement a virtual machine for Pl/0
+//|  
+//|  Notes:
+//|
+//|===========================================================================
