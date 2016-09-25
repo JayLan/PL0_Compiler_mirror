1068e8eb7766d6b46aae42d988c9c208fb572569
Jason Lancaster <jaylan.cs@knights.ucf.edu>
2016-09-25 19:41:02 -0400

    Created lexer.c shell w/notes, update collab doc for current module

---
+++ lexer.c
@@ -0,0 +1,24 @@
+//|===========================================================================
+//|  Team Name: Compiler Builders 33
+//|
+//|  Programmers:
+//|		Cristen Palmer
+//|		Megan Chipman
+//|		Jason Lancaster
+//|		Victoria Proetsch
+//|
+//|  Course:	COP3402, Fall 2016
+//|  Date:      Oct 6, 2016
+//|---------------------------------------------------------------------------
+//|  Language:	C
+//|  File:		lexer.c
+//|  Purpose:	Assignment #2, Implement a lexical analyzer for PL/0
+//|
+//|  Notes:
+//|
+//| * Identifier cannot start with a letter
+//| * Identifier cannot exceed 12 chars
+//| * Number cannot exceed 2^16 - 1
+//| * Token cannot be invalid
+//|===========================================================================
+
