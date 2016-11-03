298496349e1f96a4fb34cebc764c9da70b047c7d
chipuau <meganchipman@knights.ucf.edu>
2016-11-02 20:35:42 -0400

    Added header files.

--- lexer.h
+++ lexer.h
@@ -10,23 +10,10 @@
 //|  Date:      Sept 22, 2016
 //|---------------------------------------------------------------------------
 //|  Language:	C
-//|  File:		parser.c
-//|  Purpose:	Assignment #3, Implement a parser
+//|  File:		lexer.h
+//|  Purpose:	Provides function prototypes from lexer.c
 //|
 //|  Notes:
 //|
 //|===========================================================================
-
-//Included Libraries
-#include <stdio.h>
-#include <stdlib.h>
-#include "tokens.h"
-#include "lexer.h"
-
-int main () {
-
-
-
-}
-
 
