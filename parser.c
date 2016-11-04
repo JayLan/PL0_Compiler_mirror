a359deafea6a97b3bf425e148478e0c78fe08ecc
Jason Lancaster <jaylan.cs@knights.ucf.edu>
2016-11-04 13:39:59 -0400

    Added parser specific code to parser.c, *code is translation of grammar at this point

--- parser.c
+++ parser.c
@@ -11,7 +11,7 @@
 //|---------------------------------------------------------------------------
 //|  Language:	C
 //|  File:		parser.c
-//|  Purpose:	Assignment #3, Implement a parser
+//|  Purpose:	Assignment #3, Implement a parser and code generator for PL/0
 //|
 //|  Notes:
 //|
@@ -23,7 +23,20 @@
 #include "tokens.h"
 #include "lexer.h"
 
-int main () {
+void program();
+void block();
+void const_declaration();
+void var_declaration();
+void proc_declaration();
+void statement();
+void condition();
+void expression();
+void term();
+void factor();
+token_type relation(token_type); // return type?
+
+
+int main() {
 
     //Declare and Initialize Variables:
     aToken_type *toke;
@@ -70,5 +83,284 @@ int main () {
     //Close the cleanFile pointer
     fclose(clean);
 }
+
+
+// *** PARSER ***
+
+
+// comments
+void program(){
+	block();
+	if(tok->t != periodsym){
+		error(9);
+	}
+	advance();
+	
+}
+
+// comments
+void block(){
+	const_declaration();
+	var_declaration();
+	proc_declaration();
+	statement();
+}
+
+// comments
+void const_declaration(){
+	if(tok->t != constsym){
+		return;
+	}
+	advance();
+	
+	while(tok->t != commasym){
+		if(tok->t != identsym){
+			error(4);
+		}
+		advance();
+		
+		if(tok->t != eqsym){
+			error(3);
+		}
+		advance();
+		
+		if(tok->t != NUMBER){
+			error(2);
+		}
+		advance();
+	}
+	
+	if(tok->t != semicolonsym){
+		error(5);
+	}
+	
+	advance();
+	
+}
+
+// comments
+void var_declaration(){
+	if(tok->t != varsym){
+		return;
+	}
+	advance();
+	
+	while(tok->t != commasym){
+		if(tok->t != identsym){
+			error(4);
+		}
+		advance();
+	}
+	
+	if(tok->t != semicolonsym){
+		error(5);
+	}
+	
+	advance();
+}
+
+// comments
+void proc_declaration(){
+
+	if(tok->t != procsym){
+		return;
+	}
+	while(tok->t == procsym){
+		advance();
+		
+		if(tok->t != identsym){
+			error(4);
+		}
+		advance();
+		
+		if(tok->t != semicolonsym){
+			error(5);
+		}
+		advance();
+		block();
+		
+		if(tok->t != semicolonsym){
+			error(5);
+		}
+		advance();
+	}
+
+}
+
+// comments
+void statement(){
+
+/* 
+
+!!! do we need this? I think we are testing it in the switch !!!
+
+	if(tok->t != identsym){
+		error(); // !!! input the error code !!!
+	}
+	
+*/
+
+	switch(tok->t){
+		case identsym:
+			advance();
+			if(tok->t != becomesym){
+				error(); // !!! input the error code !!!
+			}
+			advance();
+			expression();
+			break;
+		
+		case callsym:
+			advance();
+			if(tok->t != identsym){
+				error(14);
+			}
+			advance();
+			break;
+			
+		case beginsym:
+			advance();
+			statement();
+		
+			while(tok->t == semicolonsym){
+				advance();
+				statement();
+			}
+			if(tok->t != endsym){
+				error(); // !!! input the error code !!!
+			}
+			advance();
+			break;
+		
+		case ifsym:
+			advance();
+			condition();
+		
+			if(tok->t != thensym){
+				error(16);
+			}
+			advance();
+			break;
+		
+		case whilesym:
+			advance();
+			condition();
+			if(tok->t != dosym){
+				error(18);
+			}
+			advance();
+			statement();
+			break;
+	
+		default:
+			error(7); // !!! check this !!!
+			break;
+
+		} // END switch
+
+}
+
+// comments
+void condition(){
+	if(tok->t == oddsym){
+		advance();
+		expression();
+	}else{
+		expression();
+		if(tok != relation(tok->t)){
+			error(20);
+		
+		}
+		advance();
+		expression();
+	}
+}
+
+token_type relation(token_type tok){
+	
+	switch(tok){
+		case eqsym:
+			return eqsym;
+			break;
+		case neqsym:
+			return neqsym;
+			break;
+		case lessym:
+			return lessym;
+			break;
+		case leqsym:
+			return leqsym;
+			break;
+		case gtrsym:
+			return gtrsym;
+			break;
+		case geqsym:
+			return geqsym;
+			break;
+		default:
+			error(20);
+			break;
+	}
+	
+}
+
+// comments
+void expression(){
+	if(tok->t != plussym || tok->t != minussym){
+		return;
+	}
+	
+	term();
+	
+	while(tok->t == plussym || tok->t == minussym){
+		advance();
+		term();
+	}
+	
+}
+
+// comments
+void term(){
+	factor(); // check on this
+	
+	while(tok->t == multsym || tok->t == slashsym){
+		advance();
+		factor();
+	}
+}
+
+// comments
+void factor(){
+	switch(tok->t){
+	
+		case identsym:
+			advance();
+			break;
+		
+		// !!! need to fix this: !!!
+		case NUMBER:
+			advance();
+			break;
+		
+		case "(":
+			advance();
+			expression();
+			if(tok->t != ")" ){
+				error(22);
+			}
+			advance();
+			break;
+		
+		default:
+			error(23); // !!! check this !!!
+			break;
+	
+	} // END switch
+	
+
+}
+
+
+
 
 
